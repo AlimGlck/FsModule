cc6a963c0f77561725471ff299481b5dd1f3bf76
Alim Gölcük <alimglck5421@gmail.com>
2022-11-21 02:26:39 +0300

    yeni yazım

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const fs = require('fs');
 
-fs.writeFile('employees.json','{"name": "Employee 1 Name", "salary": 2000}','utf-8',(err) => {
+setTimeout(() => {fs.writeFile('employees.json','{"name": "Employee 1 Name", "salary": 2000}','utf-8',(err) => {
     if(err) console.log(err);
     console.log("Dosya başarılı bir şekilde oluşturuldu");
-});
+})},1000);
 
-fs.readFile('employees.json','utf-8',(err,data) =>{
+
+setTimeout(() => {fs.readFile('employees.json','utf-8',(err,data) =>{
     if(err) console.log(err);
     console.log(data);
 
-})
+})},3000);
 
-fs.writeFile("employees.json",'{"name": "Murat", "salary": 5000}',(err) => {
+
+setTimeout(() => {fs.writeFile("employees.json",'{"name": "Murat", "salary": 5000}',(err) => {
     if (err) console.log("Hatalı")
   console.log('Güncellendi!');
 
-});
+})},5000);
 
-fs.unlink('employees.json',(err) => {
+setTimeout(() => {fs.unlink('employees.json',(err) => {
     if (err) console.log("Dosya silinemedi");
     console.log('Dosya silindi');
-});
+})},7000);
+
+
     
 
 
